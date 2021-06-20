4bdfc81326cdb346ce3a441098de6d8cd01a21c6
Yash Bhojgarhia <yash.bhojgarhia@jeevansathi.com>
2021-06-20 05:52:45 +0530

    JSON parsing

--- NewsFun/NewsHelper.swift
+++ NewsFun/NewsHelper.swift
@@ -12,8 +12,29 @@ class NewsHelper {
     
     func getArticles() {
         Alamofire.request("https://newsapi.org/v2/top-headlines?country=us&apiKey=09691c841c30497b83c4be63eedaecf6").responseJSON { (response) in
-            print(response)
+            if let json = response.result.value as? [String:Any] {
+                if let jsonArticles = json["articles"] as? [[String:Any]] {
+                    var articles = [Article]()
+                    for jsonArticle in jsonArticles {
+                        guard let title = jsonArticle["title"] as? String, let urlToImage = jsonArticle["urlToImage"] as? String, let url = jsonArticle["url"] as? String, let description = jsonArticle["description"] as? String else { continue }
+                        let article = Article()
+                        article.title = title
+                        article.urlToImage = urlToImage
+                        article.url = url
+                        article.description = description
+                        articles.append(article)
+                    }
+                }
+            }
         }
     }
     
+}
+
+class Article {
+    var title = ""
+    var urlToImage = ""
+    var url = ""
+    var description = ""
+    var category = ""
 }
